60a7b81ea2ef39718ac29b668592e9ca656a5c61
MatiasBS027 <153869123+MatiasBS027@users.noreply.github.com>
2025-03-11 22:31:37 +0600

    Add files via upload
    commit 1 tarea

---
+++ I SEMESTRE/Introduccion a la Programacion/Tarea2.py/2.25.py
@@ -0,0 +1,16 @@
+#
+#
+#
+#
+
+def determinarNumero(pa,pb,pc):
+    if a>b:
+        if a>c:
+            return "a es el mayor"
+        else:
+            if a==c:
+                return "a y c son los mayores"
+            else:
+                return "c es el mayor"
+        
+                

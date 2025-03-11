c41561cb1ccb02899f2223d2c0e8601aee808500
Matias <matibs0727@gmail.com>
2025-03-11 13:25:29 -0600

    commit f.01

--- I SEMESTRE/Introduccion a la Programacion/Tarea2/2.25.py
+++ I SEMESTRE/Introduccion a la Programacion/Tarea2/2.25.py
@@ -3,35 +3,50 @@
 # Fecha de ultima modificacion: 11/3/2025 12:11am
 # Version de Python: 3.13.2
 
-def determinarNumero(pa,pb,pc):
+def determinarNumero(pa, pb, pc):
+    '''
+    Funionamiento: Calcula cual de los tres numeros es el mayor
+    Entradas:
+    -pa(float):Numero a evaluar
+    -pb(float):Numero a evaluar 
+    -pc(float):Numero a evaluar
+    Salidas:
+    -a es el mayor(str): Si a es el mayor
+    -b es el mayor(str): Si b es el mayor
+    -c es el mayor(str): Si c es el mayor
+    -cualquier combinacion de los anteriores(str): Si hay empate
+    '''
 
-    if pa>pb:
-        if pa>pc:
+    if pa > pb: # Compara si 'a' es mayor que 'b'
+        if pa > pc: # Si 'a' es mayor que 'b', verifica si 'a' es mayor que 'c'
             return "a es el mayor"
         else:
-            if pa==pc:
+            if pa == pc: # Si 'a' es igual a 'c', ambos son los mayores
                 return "a y c son los mayores"
             else:
                 return "c es el mayor"
     else:
-        if pa==pb:    
-            if pa>pc:
+        if pa == pb: # Si 'a' es igual a 'b', verifica si 'a' es mayor que 'c'
+            if pa > pc:
                 return "a y b son los mayores"
             else:
-                if pa==pc:
+                if pa == pc: # Si 'a' es igual a 'c', todos son iguales
                     return "a, b y c son iguales"
                 else:
                     return "c es el mayor"
         else:
-            if pb>pc:
+            if pb > pc: # Si 'b' es mayor que 'c', 'b' es el mayor
                 return "b es el mayor"
             else:
-                if pb ==pc:
+                if pb == pc: # Si 'b' es igual a 'c', ambos son los mayores
                     return "b y c son los mayores"
                 else:
                     return "c es el mayor"
 
-a = int(input("Digite el valor de a: "))
-b = int(input("Digite el valor de b: "))
-c = int(input("Digite el valor de c: "))
-print(determinarNumero(a,b,c))
+# Solicita al usuario que ingrese los valores de 'a', 'b' y 'c'
+a = float(input("Digite el valor de a: "))
+b = float(input("Digite el valor de b: "))
+c = float(input("Digite el valor de c: "))
+
+# Imprime el resultado de la función 'determinarNumero'
+print(determinarNumero(a, b, c))
--- I SEMESTRE/Introduccion a la Programacion/Tarea2/2.27.py
+++ I SEMESTRE/Introduccion a la Programacion/Tarea2/2.27.py
@@ -3,4 +3,38 @@
 # Fecha de ultima modificacion: 11/3/2025 12:11am
 # Version de Python: 3.13.2
 
-def
+def definirNuevoSueldo(psueldo, pcategoria, phorasExtra):
+    '''
+    Funcionamiento: Calcula el nuevo sueldo de un trabajador
+    Entradas:
+    -psueldo(float):Sueldo del trabajador
+    -pcategoria(int):Categoria del trabajador
+    -phorasExtra(int):Horas extra trabajadas
+    Salidas:
+    -nuevoSueldo(float):Nuevo sueldo del trabajador
+    '''
+
+    if pcategoria == 1:
+        precioHoraExtra = 30  # El precio por hora extra es 30
+    elif pcategoria == 2:
+        precioHoraExtra = 38  # El precio por hora extra es 38
+    elif pcategoria == 3:
+        precioHoraExtra = 50  # El precio por hora extra es 50
+    elif pcategoria == 4:
+        precioHoraExtra = 70  # El precio por hora extra es 70
+    else:
+        precioHoraExtra = 0  # No hay precio por hora extra
+    if phorasExtra > 30:
+        nuevoSueldo = psueldo + (30 * precioHoraExtra)  # Solo se pagan 30 horas extra
+    else:
+        nuevoSueldo = psueldo + (phorasExtra * precioHoraExtra)  # Se pagan todas las horas extra
+
+    return nuevoSueldo
+
+# Solicita al usuario que ingrese el sueldo, la categoria y las horas extra trabajadas
+sueldo = float(input("Digite el sueldo del trabajador: "))
+categoria = int(input("Digite la categoria del trabajador:|1|2|3|4|: "))
+horasExtra = int(input("Digite las horas extra trabajadas: "))
+
+# Imprime el nuevo sueldo del trabajador
+print(f"El nuevo sueldo del trabajador es:", (definirNuevoSueldo(sueldo, categoria, horasExtra)))

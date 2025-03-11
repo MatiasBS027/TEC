fdd3e7ad754bf54cadde959b36c388c03a7c8a03
Matias <matibs0727@gmail.com>
2025-03-10 21:09:22 -0600

    uno

---
+++ I SEMESTRE/Introduccion a la Programacion/Tarea1/Tarea1/NumeroMayorMenor.py
@@ -0,0 +1,19 @@
+# Elaborado por: Matias Benavides
+# Fecha de creacion: 10/3/2025 8:00pm
+# Fecha de ultima modificacion: 10/3/2025 8:24pm
+# Version de Python: 3.13.2
+
+# Se inicializan las variables
+mayor = -100000
+menor = 100000
+i = 1
+# Se pide al usuario que digite n numeros enteros
+while i <= 12:
+    numero = int(input("Digite un numero entero: "))   
+    if numero > mayor:
+        mayor = numero
+    if numero < menor:
+        menor = numero
+    i = i + 1
+
+print("El numero mayor es: " + str(mayor) + "\nEl numero menor es: " + str(menor))  # Se imprime el numero mayor y el numero menor

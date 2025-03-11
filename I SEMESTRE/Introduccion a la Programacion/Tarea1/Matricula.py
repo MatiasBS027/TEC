84649a3aebb126e17eb5ebff4e4ba1d86de3c4de
Matias <matibs0727@gmail.com>
2025-03-10 21:45:26 -0600

    ultimo commit

--- I SEMESTRE/Introduccion a la Programacion/Tarea1/Matricula.py
+++ I SEMESTRE/Introduccion a la Programacion/Tarea1/Matricula.py
@@ -3,10 +3,10 @@
 # Fecha de ultima modificacion: 10/3/2025 7:51pm
 # Version de Python: 3.13.2
 
-matrtricula= 25224
+matrtricula= (int(input("Ingrese su matrícula: ")))   #Se pide al usuario que digite su matricula
 carrera= str(input("Ingrese la carrera a la que desea ingresar: "))   #Se pide al usuario que digite la carrera a la que desea ingresar
-semestre= 8
-promedio= float(8.7)
+semestre= int(input("Ingrese el semestre que cursa: "))   #Se pide al usuario que digite el semestre que cursa
+promedio= float(input("Ingrese su promedio: "))   #Se pide al usuario que digite su promedio
 #Se imprime la matricula, la carrera y se dice que fue aceptado
 #Tener en cuenta las tildes y las mayusculas, por que en el diagrama de flujo original vienen asi
 if carrera == "Economía":

636028413b535e4a0702dbbf96959037b567d4cf
Matias <matibs0727@gmail.com>
2025-03-17 19:24:29 -0600

    final

--- I SEMESTRE/Introduccion a la Programacion/Tarea3/DF3.19.py
+++ I SEMESTRE/Introduccion a la Programacion/Tarea3/DF3.19.py
@@ -1,46 +1,40 @@
-# Elaborado por: Matias Benavides
+# Elaborado por: Matias Benavides y Luis Carlos Tinoco Vargas
 # Fecha de creacion: 13/3/2025 6:30pm
-# Fecha de ultima modificacion: 13/3/2025 7:29pm
+# Fecha de ultima modificacion: 16/3/2025 7:29pm
 # Version de Python: 3.13.2
 
 def obtenerNumEmpleados(psueldo, pnumDelEmpleado, pmayorSueldo, pnumDelEmpleadoM):
     '''
     Funcionamiento: Compara y actualiza el mayor sueldo entre los empleados
     Entradas:
-    -pnumEmpleados(int): Numero de empleados
     -psueldo(int): Sueldo del empleado actual
-    -pi(int): Contador actual
     -pnumDelEmpleado(int): Numero del empleado actual
     -pmayorSueldo(int): Mayor sueldo encontrado hasta el momento
     -pnumDelEmpleadoM(int): Numero del empleado con mayor sueldo
     Salidas:
-    -str: Mensaje indicando el empleado con mayor sueldo y el monto
+    -El sueldo y el número del empleado con el mayor sueldo
     '''
-    if psueldo > pmayorSueldo:             # Compara el sueldo actual con el mayor
-        pmayorSueldo = psueldo             # Actualiza el mayor sueldo
-        pnumDelEmpleadoM = pnumDelEmpleado # Actualiza el número del empleado
-    
-    return ("" + str(pnumDelEmpleadoM) + "es el codigo del empleado con el mayor sueldo de: $" + str(pmayorSueldo))
+    if psueldo > pmayorSueldo:
+        return psueldo, pnumDelEmpleado
+    return pmayorSueldo, pnumDelEmpleadoM
 
 def obtenerNumEmpleadosAux(psueldo, pnumDelEmpleado, pmayorSueldoM, pnumDelEmpleadoM):
     '''
     Funcionamiento: Valida los datos de entrada antes de procesarlos
     Entradas:
-    -pnumEmpleados(int): Numero total de empleados
     -psueldo(int): Sueldo del empleado actual
-    -pi(int): Contador actual
     -pnumDelEmpleado(int): Numero del empleado actual
     -pmayorSueldoM(int): Mayor sueldo encontrado hasta el momento
     -pnumDelEmpleadoM(int): Numero del empleado con mayor sueldo
     Salidas:
-    -str: Mensaje de error si los datos son inválidos o el resultado del proceso
+    -La función principal con los parámetros validados
     '''
     # Verifica que todos los parámetros sean enteros
-    if type(psueldo) != int or type(pnumDelEmpleado) != int or type(pmayorSueldoM) != int or type(pnumDelEmpleadoM) != int:
-        return "Los cinco numeros deben ser enteros"
+    if type(psueldo) != int or type(pnumDelEmpleado) != int:
+        return None
     # Verifica que los números sean positivos
     elif psueldo <= 0:
-        return "Los numeros deben ser mayores a 0"
+        return None
     else:
         # Llama a la función principal con los parámetros validados
         return obtenerNumEmpleados(psueldo, pnumDelEmpleado, pmayorSueldoM, pnumDelEmpleadoM)
@@ -55,19 +49,23 @@ def obtenerNumEmpleadosES():
     '''
     try:
         numEmpleados = int(input("Ingrese el numero de empleados: "))
+        if numEmpleados <= 0:
+            return "El número de empleados debe ser mayor a 0" 
         mayorSueldo = 0
         numDelEmpleadoM = 0
-        i = 1
         for i in range(numEmpleados):
-            numDelEmpleado = int(input(f"Ingrese el numero del empleado "+ str(i+1)+ ": "))
-            sueldo = int(input(f"Ingrese el sueldo del empleado"+ str(i+1)+": $"))
-            obtenerNumEmpleados(sueldo, numDelEmpleado, mayorSueldo, numDelEmpleadoM)
+            numDelEmpleado = int(input(f"Ingrese el numero del empleado {i+1}: "))
+            sueldo = int(input(f"Ingrese el sueldo del empleado {i+1}: $"))
+            
+            resultado = obtenerNumEmpleadosAux(sueldo, numDelEmpleado, mayorSueldo, numDelEmpleadoM)  # Llama a la función auxiliar para procesar cada empleado
+            if resultado is None:
+                return "Los datos ingresados son inválidos"
                 
+            mayorSueldo, numDelEmpleadoM = resultado # Actualiza el mayor sueldo y el número del empleado correspondiente
+        return f"{numDelEmpleadoM} es el codigo del empleado con el mayor sueldo de: ${mayorSueldo}" # Retorna el resultado final
+    
     except ValueError:
         return "Los datos deben ser numeros enteros"
-    else:
-        return obtenerNumEmpleadosAux(sueldo, numDelEmpleado, mayorSueldo, numDelEmpleadoM)
 
 # Inicia la ejecución del programa
 print(obtenerNumEmpleadosES())  # Imprime el resultado
-

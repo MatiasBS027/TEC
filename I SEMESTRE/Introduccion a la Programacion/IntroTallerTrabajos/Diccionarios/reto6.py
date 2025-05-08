bde37101c8fe0782a00060a5f40c24331b26994f
Matias <matibs0727@gmail.com>
2025-05-08 08:42:51 -0600

    diccionario

--- I SEMESTRE/Introduccion a la Programacion/IntroTallerTrabajos/Diccionarios/reto6.py
+++ I SEMESTRE/Introduccion a la Programacion/IntroTallerTrabajos/Diccionarios/reto6.py
@@ -1,2 +1,32 @@
-def separarPalabras():
-    
+import re
+
+def separarPalabras(cadena):
+    palabra = cadena.split()
+    return palabra
+
+def crearIndice(palabra):
+    palabra = separarPalabras(palabra)
+    diccionario = {}
+    for i in palabra:
+        inicial = i[0]
+        if inicial not in diccionario:
+            diccionario[inicial] = [i]
+        elif i not in diccionario[inicial]:
+            diccionario[inicial].append(i)
+    diccionario = ordenar(diccionario)
+    return diccionario  
+
+def ordenar(diccionario):
+    for i in diccionario:
+        diccionario[i].sort()
+        diccionarioOrdenado = dict(sorted(diccionario.items()))
+    return diccionarioOrdenado
+
+def validar(cadena):
+    if cadena == "":
+        return "Debe ingresar información para poder crear el diccionario alfabético."
+    else:
+        return crearIndice(cadena)
+
+cadena = input("Ingrese una cadena de texto: ").lower()
+print(validar(cadena))

24a54efe422dcc7d0504a964efe8f160c1f91ca0
Matias <matibs0727@gmail.com>
2025-08-11 22:30:38 -0600

    proyecto1

--- II SEMESTRE/Estructuras de Datos/ProyectosC/struct.c
+++ II SEMESTRE/Estructuras de Datos/ProyectosC/struct.c
@@ -3,25 +3,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Persona {
+typedef struct { // El typedef nos ayuda a crear un alias para la estructura y asi no tener que usar 'struct' cada vez que la queramos usar
     char name[50];
     int age;
     float height;
-};
+}Persona; // Como hemos definido la estructura, ahora podemos usar 'Persona' como un tipo de dato e incluso podrias tener un struct vacio ya que llama al "apodo" 'Persona'
+
+void print_persona(Persona *p) {
+    printf("Nombre: %s\n", p->name); // El operador '->' se usa para acceder a los miembros de la estructura a través de un puntero
+    printf("Edad: %d\n", p->age); // Que lo mismo es igual a (*p).age
+    printf("Altura: %.2f\n", p->height);    
+}
 
 int main(){
-    struct Persona p1;
-    strcpy(p1.name, "Matias");
-    p1.age = 20;
-    p1.height = 1.75;
+    Persona p1;
+    
+    strcpy(p1.name, "Matias"); // Aqui ponemos el p1.name para que sea igual a "Matias"
+    p1.age = 20; // En vez de usar un puntero, asignamos directamente el valor a la estructura
+    p1.height = 1.75;// Y no usamos el p-> para acceder a los miembros de la estructura, sino que usamos el punto (.)
 
-    printf("Nombre: %s\n", p1.name);
-    printf("Edad: %d\n", p1.age);
-    printf("Altura: %.2f\n", p1.height);
-
-
-
-
+    print_persona(&p1); // Llama a la función para imprimir los datos de la persona
 
     return 0; // Fin
 }

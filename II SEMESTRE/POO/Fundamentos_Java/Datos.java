c5ac229fc948f0e05f8f166382597f725444405b
Matias <matibs0727@gmail.com>
2025-08-11 22:48:59 -0600

    JAVA1

---
+++ II SEMESTRE/POO/Fundamentos_Java/Datos.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // Este es un comentario en Java
+        //System.out.println("Hello, World!");
+
+        String message = "Hello, World!";
+        System.out.println(message);
+        String telf = "506-1023-1312";
+        System.out.println(telf);
+        int number = 42;
+        System.out.println("El número es: " + number);
+        double pi = 3.14159;
+        System.out.println("El valor de pi es: " + pi);
+        boolean isJavaFun = true;
+        System.out.println("¿Es Java divertido? " + isJavaFun);
+        boolean Desautorizado = false;
+        System.out.println(Desautorizado);
+    }
+} 

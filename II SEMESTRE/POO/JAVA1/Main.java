cd0b8454b2e8fabd33a6a377b808f6fd28383a91
Matias <matibs0727@gmail.com>
2025-08-14 00:36:08 -0600

    mas

--- II SEMESTRE/POO/JAVA1/Main.java
+++ II SEMESTRE/POO/JAVA1/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Lenguaje java = new Lenguaje("Java", "1995");
-        Lenguaje python = new Lenguaje("Python", "1991");
-        Lenguaje cSharp = new Lenguaje("C#", "2000");
+        Libro libro = new Libro("Cien años de soledad", "Gabriel García Márquez");
+        libro.description();
 
-        java.description();
-        python.description();
-        cSharp.description();
     }
 }

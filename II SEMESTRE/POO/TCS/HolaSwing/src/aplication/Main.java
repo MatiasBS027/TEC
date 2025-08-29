0c6a6b6eb4fc5078e5012c48f8974ef14b89df70
Matias <matibs0727@gmail.com>
2025-08-29 09:30:03 -0600

    Tarea02 y 03

---
+++ II SEMESTRE/POO/TCS/HolaSwing/src/aplication/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplication;
+
+import ventanas.Hola;
+
+/**
+ *
+ * @author Matias
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Hola miventana = new Hola();
+        
+        miventana.setVisible(true);
+    }
+    
+}

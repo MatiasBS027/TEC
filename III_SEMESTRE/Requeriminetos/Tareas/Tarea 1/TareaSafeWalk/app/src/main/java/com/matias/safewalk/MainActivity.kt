62277181aaa1d08a7d7ce8739fd46e5d2e9dcf89
Matias <matibs0727@gmail.com>
2026-03-03 18:23:43 -0600

    Tarea 1 y 2 de reque

---
+++ III_SEMESTRE/Requeriminetos/Tareas/Tarea 1/TareaSafeWalk/app/src/main/java/com/matias/safewalk/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.matias.safewalk
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.matias.safewalk.ui.theme.SafeWalkTheme
+import android.Manifest
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ProcessLifecycleOwner
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var viewModel: SensorViewModel
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        pedirPermisoSms()
+
+        val dataStoreManager = DataStoreManager(this)
+        viewModel = ViewModelProvider(
+            this,
+            SensorViewModelFactory(application, dataStoreManager)
+        )[SensorViewModel::class.java]
+        setContent {
+            SafeWalkTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
+                    NavGraph(viewModel = viewModel,
+                        dataStoreManager = dataStoreManager
+                    )
+                }
+            }
+        }
+
+        ProcessLifecycleOwner.get().lifecycle.addObserver(
+            object : DefaultLifecycleObserver {
+
+                override fun onStart(owner: LifecycleOwner) {
+                    // la app está en primer plano
+                    if (viewModel.modoVigilancia.value) {
+                        viewModel.iniciarSensores()
+                    }
+                }
+
+                override fun onStop(owner: LifecycleOwner) {
+                    // la app se fue a segundo plano (pero sigue abierta)
+                    if (viewModel.modoVigilancia.value) {
+                        viewModel.iniciarSensores()
+                    } else {
+                        viewModel.detenerSensores()
+                    }
+                }
+            }
+        )
+    }
+    override fun onResume() {
+        super.onResume()
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        accelerometer?.let {
+            sensorManager.registerListener(viewModel, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.unregisterListener(viewModel)
+    }
+
+    private fun pedirPermisoSms() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.SEND_SMS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.SEND_SMS),
+                1
+            )
+        }
+    }
+
+}

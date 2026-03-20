cf0ac0cdf3870c1666d311d347a1a7e7bb599b6a
Matias <matibs0727@gmail.com>
2026-03-19 20:35:57 -0600

    avance de la Tarea 1 de BD

---
+++ III_SEMESTRE/BasesDeDatos/Tarea1/README.md
@@ -0,0 +1,10 @@
+# BD1 - Primera Tarea Programada
+
+ITCR | Escuela de Ingeniería en Computación | Base de Datos 1
+
+Prueba de concepto: aplicación web para gestionar empleados,
+conectada a MS SQL Server mediante procedimientos almacenados.
+
+**Stack:** Node.js · TypeScript · Express · MS SQL Server · Hamachi
+
+**Integrantes:**  Matias Benavides Sandoval / Sebastián Ramírez Abarca
---
+++ III_SEMESTRE/BasesDeDatos/Tarea1/SQL/Scripts/Empleados.sql
@@ -0,0 +1,122 @@
+USE EmpleadosDB;
+GO
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Juan Perez', 200000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Sebastian Ramirez', 500000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Marco Nunez', 400000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Amanda Watson', 250000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Daniel Solano', 375000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Fernando Aguilar', 400000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Samantha Flores', 800000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Jesus Villalobos', 100000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Valentina Leandro', 970000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Javier Contreras', 200000.00);
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Marcela Mora', 700000.00);
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Ana Abarca', 600000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Andres Pacheco', 225000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Franco Lopez', 580000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Tamara Ramirez', 850000.00);
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Ximena Conejo', 770000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Matias Fernandez', 637000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Jorge Hernandez', 200000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Mercedes Beinz', 330000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Santiago Vives', 450000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Maria Rojas', 55000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Daniela Salas', 346000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Fernanda Chinchilla', 236000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Francisco Rodriguez', 745000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Adam Warlock', 886000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Eva Garcia', 274000.00);
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Rafael Fello', 936000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Isabel Meza', 875000.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Nohelia Pereira', 347050.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Jose Mata', 200000.00);
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Diego Rojas', 762700.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Josimar Alpizar', 649300.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Valeria Badilla', 603700.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Cristian Brenes', 806300.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Axel Quesada', 374500.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Ariel Martinez', 892630.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Fabian Perez', 937600.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Nicole Campos', 773530.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Dayana Angulo', 633500.00); 
+
+INSERT dbo.Empleado (Nombre, Salario)
+VALUES ('Johana Sanchez', 973650.00); 
